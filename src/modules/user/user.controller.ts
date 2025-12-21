d43e69eea199a031ac7187902cceacdab96d671d
Lamriver123 <ngoclamhttv@gmail.com>
2025-12-21 15:30:07 +0700

    multer

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -8,7 +8,6 @@ import { UpdateFcmTokenDto } from './dto/update-fcm-token.dto';
 import { FileFieldsInterceptor, FileInterceptor } from '@nestjs/platform-express';
 import { storage } from '../cloudinary/cloudinary.storage';
 import { Public } from 'src/common/decorators/public.decorator';
-import { File } from 'multer';
 import { RolesGuard } from 'src/common/guards/roles.guard';
 import { UserRole } from 'src/shared/enums/user_role';
 import { Roles } from 'src/common/decorators/role.decorator';
