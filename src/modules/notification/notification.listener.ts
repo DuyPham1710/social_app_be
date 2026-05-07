e8f1d0a16f9b6c12b18023fd4e15a6523c48ef96
Lamriver123 <ngoclamhttv@gmail.com>
2026-05-07 23:51:29 +0700

    fix notification community

--- src/modules/notification/notification.listener.ts
+++ src/modules/notification/notification.listener.ts
@@ -119,7 +119,8 @@ export class NotificationListener {
       type: NotificationType.COMMUNITY_POST_PENDING,
       targetId: payload.postId,
       message: ` gửi yêu cầu đăng bài vào cộng đồng "${payload.communityName}" cần bạn phê duyệt`,
-      content: payload.communityId,
+      content: payload.communityName,
+      communityId: payload.communityId,
     });
   }
 
@@ -129,9 +130,10 @@ export class NotificationListener {
       receiver: payload.receiver,
       sender: payload.sender,
       type: NotificationType.COMMUNITY_JOIN_REQUEST,
-      targetId: payload.communityId,
+      targetId: payload.requestId,
       message: ` đã gửi yêu cầu tham gia cộng đồng "${payload.communityName}"`,
-      content: payload.communityId,
+      content: payload.communityName,
+      communityId: payload.communityId,
     });
   }
 
@@ -143,7 +145,8 @@ export class NotificationListener {
       type: NotificationType.COMMUNITY_JOIN_REQUEST,
       targetId: payload.communityId,
       message: ` đã tham gia cộng đồng "${payload.communityName}"`,
-      content: payload.communityId,
+      content: payload.communityName,
+      communityId: payload.communityId,
     });
   }
 
@@ -154,8 +157,9 @@ export class NotificationListener {
       sender: payload.sender,
       type: NotificationType.COMMUNITY_JOIN_APPROVED,
       targetId: payload.communityId,
-      message: ` đã phê duyệt yêu cầu tham gia cộng đồng "${payload.communityName}"`,
-      content: payload.communityId,
+      message: `Admin đã chấp nhận yêu cầu tham gia cộng đồng`,
+      content: payload.communityName,
+      communityId: payload.communityId,
     });
   }
 
@@ -166,8 +170,9 @@ export class NotificationListener {
       sender: payload.sender,
       type: NotificationType.COMMUNITY_JOIN_REJECTED,
       targetId: payload.communityId,
-      message: ` đã từ chối yêu cầu tham gia cộng đồng "${payload.communityName}"`,
-      content: payload.communityId,
+      message: `Admin đã từ chối yêu cầu tham gia cộng đồng`,
+      content: payload.communityName,
+      communityId: payload.communityId,
     });
   }
 
@@ -179,7 +184,8 @@ export class NotificationListener {
       type: NotificationType.COMMUNITY_INVITE,
       targetId: payload.communityId,
       message: ` đã mời bạn tham gia cộng đồng "${payload.communityName}"`,
-      content: payload.communityId,
+      content: payload.communityName,
+      communityId: payload.communityId,
     });
   }
 
@@ -191,7 +197,8 @@ export class NotificationListener {
       type: NotificationType.COMMUNITY_POST_APPROVED,
       targetId: payload.postId,
       message: ` đã duyệt bài viết của bạn trong cộng đồng "${payload.communityName}"`,
-      content: payload.communityId,
+      content: payload.communityName,
+      communityId: payload.communityId,
     });
   }
 
@@ -203,7 +210,8 @@ export class NotificationListener {
       type: NotificationType.COMMUNITY_POST_REJECTED,
       targetId: payload.postId,
       message: ` đã từ chối bài viết của bạn trong cộng đồng "${payload.communityName}"`,
-      content: payload.communityId,
+      content: payload.communityName,
+      communityId: payload.communityId,
     });
   }
 
